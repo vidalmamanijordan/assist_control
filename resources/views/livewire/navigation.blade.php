bb47cda40d39b11805c5c8dd696401ea43576f57
vidalmamanijordan <vidal_mamani@upeu.edu.pe>
2023-11-09 10:11:49 -0500

    Register

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -96,6 +96,7 @@
                     <a href="{{ route('register') }}" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">
                         Register
                     </a>
+                    
                 </div>
             @endauth
         </div>
