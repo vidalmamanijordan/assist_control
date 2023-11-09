a3086ae4413bf1a5cc631d56153789a953d81057
vidalmamanijordan <vidal_mamani@upeu.edu.pe>
2023-11-09 09:16:03 -0500

    Limpiando vite en layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,6 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css" integrity="sha512-vKMx8UnXk60zUwyUnUPM3HbQo8QfmNx7+ltw8Pm5zLusl1XIfwcxo8DbWCqMGKaWeNxWA8yrx5v3SaVpMvR3CA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
-        @vite(['resources/views/admin/participants/styles.css', 'resources/viewa/admin/participants/styles.css'])
 
         <!-- Styles -->
         @livewireStyles
