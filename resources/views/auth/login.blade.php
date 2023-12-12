f31c208c09e18bd2708cbe5f8be98e0277c1ebd0
vidalmamanijordan <vidal_mamani@upeu.edu.pe>
2023-12-12 18:04:55 -0500

    Oficil logo in login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,7 +3,7 @@
         <x-slot name="logo">
             <a href="/">
                 <svg class="w-16 h-16" viewbox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <img class="h-12 w-auto" src="{{ asset('img/logos/dti_oficial.png') }}" alt="dti">
+                    <img class="h-12 w-auto" src="{{ asset('img/logos/vijorma_oficial.png') }}" alt="vijorma">
                 </svg>
             </a>
         </x-slot>
